b7eec65f9416beefcdeb857a50a74f61566bb124
Muhammedminhaj798 <muhammedminhaj798@gmail.com>
2025-05-16 09:58:06 +0530

    update

--- src/controller/admin/adminProductController.js
+++ src/controller/admin/adminProductController.js
@@ -6,9 +6,12 @@ const getAllProducts = async (req, res, next) => {
   const products = await productSchema.find();
 
   if (!products) {
+
     return next(new customError("products not found", 404));
+
   }
   res.status(200).json({
+    
     status: "success",
     message: "All product get successfully",
     data: products,
