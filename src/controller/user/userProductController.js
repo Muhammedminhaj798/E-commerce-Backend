6d9dc3e29c9cee1f3a4c946552165d4bcb4aeb7c
Muhammedminhaj798 <muhammedminhaj798@gmail.com>
2025-05-22 22:46:47 +0530

    update

--- src/controller/user/userProductController.js
+++ src/controller/user/userProductController.js
@@ -1,7 +1,7 @@
 import productSchema from "../../model/productSchema.js";
 
 const getAllProduct = async (req, res) => {
-  const allProduct = await productSchema.find({isDeleted:false});
+  const allProduct = await productSchema.find({ isDeleted: false });
 
   res.status(200).json({
     message: "All Product get successfully",
@@ -12,7 +12,10 @@ const getAllProduct = async (req, res) => {
 
 const getProductType = async (req, res) => {
   const type = req.params.type;
-  const productType = await productSchema.find({ type: type ,isDeleted:false});
+  const productType = await productSchema.find({
+    type: type,
+    isDeleted: false,
+  });
   res.status(200).json({
     message: "product get by type succesfully",
     status: "success",
@@ -22,7 +25,7 @@ const getProductType = async (req, res) => {
 
 const getProductById = async (req, res) => {
   const id = req.params.id;
-  
+
   const productId = await productSchema.findById(id);
   // console.log(productId)
   res.status(200).json({
@@ -33,16 +36,19 @@ const getProductById = async (req, res) => {
 };
 
 const getFilterProduct = async (req, res) => {
-  const { lowerprice,upperprice, brand, type } = req.body;
+  const { lowerprice, upperprice, brand, type } = req.body;
   if (brand) {
-    const product = await productSchema.find({brand : brand,isDeleted:false});
+    const product = await productSchema.find({
+      brand: brand,
+      isDeleted: false,
+    });
     res.status(200).json({
       error: false,
       product,
     });
   }
   if (type) {
-    const product = await productSchema.find({type : type,isDeleted:false});
+    const product = await productSchema.find({ type: type, isDeleted: false });
     res.status(200).json({
       error: false,
       product,
@@ -50,7 +56,8 @@ const getFilterProduct = async (req, res) => {
   }
   if (lowerprice && upperprice) {
     const products = await productSchema.find({
-      price: { $gte: lowerprice, $lte: upperprice },isDeleted:false
+      price: { $gte: lowerprice, $lte: upperprice },
+      isDeleted: false,
     });
     return res.status(200).json({
       error: false,
@@ -60,23 +67,30 @@ const getFilterProduct = async (req, res) => {
 };
 
 const getSearchProduct = async (req, res) => {
-    const { search } = req.body;
-  
-    if (search) {
-      const products = await productSchema.find({
-        name: { $regex: search, $options: 'i' },isDeleted:false
-      });
-  
-      return res.status(200).json({
-        error: false,
-        products,
-      });
-    }
-  
-    res.status(400).json({
-      error: true,
-      message: "No search term provided",
-    });
-  };
+  const { search } = req.body;
 
-export { getAllProduct, getProductType, getProductById, getFilterProduct, getSearchProduct };
+  if (search) {
+    const products = await productSchema.find({
+      name: { $regex: search, $options: "i" },
+      isDeleted: false,
+    });
+
+    return res.status(200).json({
+      error: false,
+      products,
+    });
+  }
+
+  res.status(400).json({
+    error: true,
+    message: "No search term provided",
+  });
+};
+
+export {
+  getAllProduct,
+  getProductType,
+  getProductById,
+  getFilterProduct,
+  getSearchProduct,
+};
