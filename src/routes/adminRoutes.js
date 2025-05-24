840ca679efdf1e74d93953eef80d9e2609a8b886
Muhammedminhaj798 <muhammedminhaj798@gmail.com>
2025-05-24 20:55:34 +0530

    update

--- src/routes/adminRoutes.js
+++ src/routes/adminRoutes.js
@@ -1,32 +1,43 @@
-import express from "express"
-import tryCatch from "../middleware/tryCatch.js"
-import { addProduct, deleteProduct, editProduct, getAllProducts, getProductById } from "../controller/admin/adminProductController.js"
-import { viewAllUsers, viewUserById } from "../controller/admin/adminUsersController.js"
-import upload from "../middleware/imageupload.js"
-import { getAdmiOrderDetails } from "../controller/admin/adminOrderController.js"
-import { updateOrderStatus } from "../controller/admin/updateOrderStatus.js"
-import { blockUser, unblockUser } from "../controller/admin/blockedUSerController.js"
+import express from "express";
+import tryCatch from "../middleware/tryCatch.js";
+import {
+  addProduct,
+  deleteProduct,
+  editProduct,
+  getAllProducts,
+  getProductById,
+} from "../controller/admin/adminProductController.js";
+import {
+  viewAllUsers,
+  viewUserById,
+} from "../controller/admin/adminUsersController.js";
+import upload from "../middleware/imageupload.js";
+import { getAdmiOrderDetails } from "../controller/admin/adminOrderController.js";
+import { updateOrderStatus } from "../controller/admin/updateOrderStatus.js";
+import {
+  blockUser,
+  unblockUser,
+} from "../controller/admin/blockedUSerController.js";
 
-const adminRouter = express.Router()
+const adminRouter = express.Router();
 
 adminRouter
-//get allProducts/addProductd/deleteProduct/specific product/edit product
-.get("/adminProduct",tryCatch(getAllProducts))
-.post("/addProduct",upload.single('image'),tryCatch(addProduct))
-.get("/admingetProduct/:id",tryCatch(getProductById))
-.put("/productDeleted/:id",tryCatch(deleteProduct))
-.put("/editProduct/:id",upload.single('image'),tryCatch(editProduct))
-.get("/getOrderDetails",tryCatch(getAdmiOrderDetails))
-.put('/orderStatus/:orderId', tryCatch(updateOrderStatus))
+  //get allProducts/addProductd/deleteProduct/specific product/edit product
+  .get("/adminProduct", tryCatch(getAllProducts))
+  .post("/addProduct", upload.single("image"), tryCatch(addProduct))
+  .get("/admingetProduct/:id", tryCatch(getProductById))
+  .put("/productDeleted/:id", tryCatch(deleteProduct))
+  .put("/editProduct/:id", upload.single("image"), tryCatch(editProduct))
+  .get("/getOrderDetails", tryCatch(getAdmiOrderDetails))
+  .put("/orderStatus/:orderId", tryCatch(updateOrderStatus))
 
-//users routes
-.put('/user/:id/block',tryCatch(blockUser))
-.put('/user/:id/unblock',tryCatch(unblockUser))
-.get('/user/blocked', tryCatch(unblockUser))
+  //users routes
+  .put("/user/:id/block", tryCatch(blockUser))
+  .put("/user/:id/unblock", tryCatch(unblockUser))
+  .get("/user/blocked", tryCatch(unblockUser))
 
-//view all users/get a specific user
-.get("/usersList",tryCatch(viewAllUsers))
-.get("/getuser/:id",tryCatch(viewUserById))
+  //view all users/get a specific user
+  .get("/usersList", tryCatch(viewAllUsers))
+  .get("/getuser/:id", tryCatch(viewUserById));
 
-
-export default adminRouter
+export default adminRouter;
