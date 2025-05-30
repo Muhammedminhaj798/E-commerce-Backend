b9fc0c3ae5c507cf7ae8adcccdb7b40c8529b9b5
Muhammedminhaj798 <muhammedminhaj798@gmail.com>
2025-05-30 09:59:40 +0530

    update

--- src/controller/admin/adminUsersController.js
+++ src/controller/admin/adminUsersController.js
@@ -1,28 +1,27 @@
-import User from "../../model/userSchema.js"
-import customError from "../../utils/customErr.js"
+import User from "../../model/userSchema.js";
+import customError from "../../utils/customErr.js";
 
-const viewAllUsers = async(req,res,next) => {
-    const users = await User.find().select("name email")
+const viewAllUsers = async (req, res, next) => {
+  const users = await User.find().select("name email");
 
-    
-    if(!users){
-        return next(new customError("users not found",404))
-    }
-    res.status(200).json({
-        message:"User details is success",
-        status:"success",
-        data:users
-    })
-}
+  if (!users) {
+    return next(new customError("users not found", 404));
+  }
+  res.status(200).json({
+    message: "User details is success",
+    status: "success",
+    data: users,
+  });
+};
 
-const viewUserById = async(req,res,next) => {
-    const _id = req.params.id
-    const userId = await User.findById(_id)
-    res.status(200).json({
-        message:"user get by id successfully",
-        status:"success",
-        data:userId
-    })
-}
+const viewUserById = async (req, res, next) => {
+  const _id = req.params.id;
+  const userId = await User.findById(_id);
+  res.status(200).json({
+    message: "user get by id successfully",
+    status: "success",
+    data: userId,
+  });
+};
 
-export {viewAllUsers, viewUserById}
+export { viewAllUsers, viewUserById };
